67f5a45eaa986b47a2127fa2dd2344e5f3d9031c
osoken <osoken@osoekawalab.com>
2026-02-01 11:09:11 +0900

    fix: enhance type hints for before_scenario and after_scenario functions

--- e2e/stubs/getgauge/python.pyi
+++ e2e/stubs/getgauge/python.pyi
@@ -1,11 +1,17 @@
 from collections.abc import Callable
-from typing import TypeVar
+from typing import TypeVar, overload
 
 _F = TypeVar("_F")
 
 def step(pattern: str) -> Callable[[_F], _F]: ...
+@overload
 def before_scenario(func: _F) -> _F: ...
+@overload
+def before_scenario(tag: str) -> Callable[[_F], _F]: ...
+@overload
 def after_scenario(func: _F) -> _F: ...
+@overload
+def after_scenario(tag: str) -> Callable[[_F], _F]: ...
 
 class _DataStore:
     scenario: dict[str, object]
